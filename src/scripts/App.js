6fab182565149bc3415f57c8bc524a292d1c5394
Lorenzo-Rios <lorenzorios2003@gmail.com>
2024-04-01 19:29:28 -0300

    terminamos

---
+++ src/scripts/App.js
@@ -0,0 +1,12 @@
+import '../css/App.css';
+import NavBar from '../Components/NavBar/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <>
+      <NavBar/>
+    </>
+  );
+}
+
